40de5229b68f48f772c708692ea8160ad406bbe2
shafayat783593 <sshapa17@gmail.com>
2025-01-09 00:22:25 +0600

    first code

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 Shery.mouseFollower();
 Shery.makeMagnet(".icon");
+Shery.hoverWithMediaCircle(".personal-info h1", {
+  videos: [
+    "https://cdn.cuberto.com/cb/projects/kzero/cover.mp4?2",
+    // "https://cdn.cuberto.com/cb/home/featured/header.mp4?2",
+    // "https://cdn.cuberto.com/cb/home/hero/1.mp4",
+  ],
+});
